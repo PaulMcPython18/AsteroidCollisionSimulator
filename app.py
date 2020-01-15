99783f668e1d364720f047814de766d9a3bc4894
PaulMcPython18 <57735592+PaulMcPython18@users.noreply.github.com>
2020-01-14 20:52:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, sessions, session
 import time
 import os
 app = Flask(__name__)
-app.secret_key = os.urrandom(24)
+app.secret_key = os.urandom(24)
 @app.route('/')
 def index():
     return render_template('index.html')
